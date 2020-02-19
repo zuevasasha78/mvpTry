df436382fb88e8244d88c40c0ea77ca7ae81ed48
a.i.zueva <a.i.zueva@tinkoff.ru>
2020-02-19 17:32:20 +0300

    log

--- app/src/main/java/com/example/mvptry/mvp/UserActivity.kt
+++ app/src/main/java/com/example/mvptry/mvp/UserActivity.kt
@@ -2,6 +2,7 @@ package com.example.mvptry.mvp
 
 import android.app.ProgressDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -38,19 +39,35 @@ class UserActivity : AppCompatActivity() {
     private fun init() {
         editTextName = findViewById(R.id.name)
         editTextEmail = findViewById(R.id.email)
+        Log.d("tag", "get view by id")
         findViewById<Button>(R.id.add).setOnClickListener { presenter.add() }
         findViewById<Button>(R.id.clear).setOnClickListener { presenter.clear() }
+        Log.d("tag", "set listener on button")
+
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         userAdapter = UserAdapter()
+        Log.d("tag", "create adapter")
+
         val userList: RecyclerView = findViewById(R.id.list)
         userList.layoutManager = layoutManager
         userList.adapter = userAdapter
         val dbHelper = DbHelper(this)
+        Log.d("tag", "dbHelper init")
+
         val usersModel = UserModel(dbHelper)
+        Log.d("tag", "usersModel init")
+
         presenter = UserPresenter(usersModel)
+        Log.d("tag", "UserPresenter init")
+
+
         presenter.attachView(this)
+        Log.d("tag", "attachView")
+
         presenter.viewIsReady()
+        Log.d("tag", "viewIsReady")
+
     }
 
     fun getUserData(): UserData {
